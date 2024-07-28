41097e89e6eb72b55ea9900dd6be8cd3d9929866
soumyajit0 <soumyajitdeysarkar@gmail.com>
2024-07-28 15:00:48 +0530

    Proxy Pattern

---
+++ Week 1_Design Patterns and Principles/ProxyPatternExample/src/ProxyPatternDemo.java
@@ -0,0 +1,21 @@
+import dependencies.Image;
+import dependencies.ProxyImage;
+
+public class ProxyPatternDemo {
+    public static void main(String[] args) {
+        Image image1 = new ProxyImage("img1.png");
+        Image image2 = new ProxyImage("img2.png");
+
+        // Image will be loaded and displayed
+        image1.display();
+        // Image will be displayed without loading or caching
+        image1.display();
+
+        System.out.println();
+
+        // Image will be loaded and displayed
+        image2.display();
+        // Image will be displayed without loading or caching
+        image2.display();
+    }
+}
---
+++ Week 1_Design Patterns and Principles/ProxyPatternExample/src/dependencies/Image.java
@@ -0,0 +1,5 @@
+package dependencies;
+
+public interface Image {
+    void display();
+}
